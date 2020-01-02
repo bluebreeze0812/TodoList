28b6a57373eda1803ea76eb537400b1aee0553fb
Leo Wang <bluebreeze0812@outlook.com>
2020-01-02 16:19:23 +0800

    add todos and todoItem components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Todos from './components/Todos'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'todo 1',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'todo 2',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'todo 3',
+        completed: false
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    )
+  }
 }
 
 export default App;
