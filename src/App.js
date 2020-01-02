fcb98af1b989d7c0432607d09295cd435d32d39f
Leo Wang <bluebreeze0812@outlook.com>
2020-01-02 16:43:29 +0800

    add checkbox listeners

--- src/App.js
+++ src/App.js
@@ -18,15 +18,26 @@ class App extends Component {
       {
         id: 3,
         title: 'todo 3',
-        completed: false
+        completed: true
       }
     ]
+  }
+
+  toggleComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} />
       </div>
     )
   }
